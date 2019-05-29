b7b60ee029e9966f930884e116e48a1b26de9c88
mhdalmasri <m.almasri@outlook.de>
2019-05-29 11:47:59 +0200

    state added

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,34 @@ import React from "react";
 import Header from "./Header";
 import ToDoList from "./ToDoList";
 import ToDoForm from "./ToDoForm";
+import uuid from "uuid/v4";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      toDoItems: {}
+    }
+  }
+  addToDo = text => {
+    const todo = {
+      uuid: uuid(),
+      text: text,
+      done: false
+    };
+
+    this.setState(state => {
+      state.toDoItems[todo.uuid] = todo;
+      return state;
+    });
+  };
+
   render() {
     return (
       <div className="container">
         <Header tagline="Here are all the next tasks." />
-        <ToDoForm />
+        <ToDoForm addToDo={this.addToDo} />
         <ToDoList />
       </div>
     );
