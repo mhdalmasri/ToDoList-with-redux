2e502ce0d060f64b401d19a94b29880cd2a61443
mhdalmasri <m.almasri@outlook.de>
2019-05-31 19:31:18 +0200

    i

---
+++ build/precache-manifest.90f231dfcfa95a7cafad57aeefdd9825.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f91d4e39da4697b3a48d3c1e4737189",
+    "url": "/ToDoList-with-redux/index.html"
+  },
+  {
+    "revision": "e14be8913a549895eb83",
+    "url": "/ToDoList-with-redux/static/css/2.d98a2158.chunk.css"
+  },
+  {
+    "revision": "0382196209a2a5c59298",
+    "url": "/ToDoList-with-redux/static/css/main.2abe7fe0.chunk.css"
+  },
+  {
+    "revision": "e14be8913a549895eb83",
+    "url": "/ToDoList-with-redux/static/js/2.cbd500bf.chunk.js"
+  },
+  {
+    "revision": "0382196209a2a5c59298",
+    "url": "/ToDoList-with-redux/static/js/main.658f8a81.chunk.js"
+  },
+  {
+    "revision": "c8c2c1e6c47342a88e22",
+    "url": "/ToDoList-with-redux/static/js/runtime~main.19b702ea.js"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/ToDoList-with-redux/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/ToDoList-with-redux/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/ToDoList-with-redux/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/ToDoList-with-redux/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/ToDoList-with-redux/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/ToDoList-with-redux/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/ToDoList-with-redux/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/ToDoList-with-redux/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/ToDoList-with-redux/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/ToDoList-with-redux/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/ToDoList-with-redux/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/ToDoList-with-redux/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/ToDoList-with-redux/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/ToDoList-with-redux/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/ToDoList-with-redux/static/media/fa-solid-900.f14c3b2f.ttf"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mhdalmasri/ToDoList-with-redux/precache-manifest.2a5a8ac7509687612a75cb8f6b435827.js"
+  "/ToDoList-with-redux/precache-manifest.90f231dfcfa95a7cafad57aeefdd9825.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mhdalmasri/ToDoList-with-redux/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ToDoList-with-redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
